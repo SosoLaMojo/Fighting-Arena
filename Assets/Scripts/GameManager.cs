78de8d2b3052b88d0f7c2ba6a09d18ca2a3a03ae
Solange <sol.sch1990@gmail.com>
2020-02-10 19:32:43 +0100

    Added sprites for UI + GameManager script + MenuManager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void LoadStartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+        Time.timeScale = 1;
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 0;
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject panelMenuStartGame;
+    public void ActivatePanelMenuStartGame()
+    {
+        panelMenuStartGame.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void DesactivatePanelMenuStartGame()
+    {
+        panelMenuStartGame.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
