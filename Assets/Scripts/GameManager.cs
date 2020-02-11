72ebd745f22571e007c367d230900fa5df519a24
Vincent <skriosdreys@gmail.com>
2020-02-11 16:21:53 +0100

    Adding 2 player controls

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,78 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
+public enum ChoicesStates {P1TURN, P2TURN}
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+    
+    private ChoicesStates state;
+    
+    [SerializeField] private GameObject Adventurer;
+    [SerializeField] private GameObject Gladiator;
+
+    private bool hasChosen = false;
+    private bool P1ChoseAdventurer;
+    private bool P2ChoseAdventurer;
+
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case ChoicesStates.P1TURN:
+            {
+                if (hasChosen)
+                {
+                    state = ChoicesStates.P2TURN;
+
+                    hasChosen = false;
+                }
+                break;
+            }
+            case ChoicesStates.P2TURN:
+            {
+                if (hasChosen)
+                {
+                    LoadScene("SceneVincent");
+                }
+                break;
+            }
+        }
+    }
+
+
     public void LoadScene(string sceneName)
     {
         Time.timeScale = 1;
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void GladiatorChoice()
+    {
+        if (state == ChoicesStates.P1TURN)
+        {
+            P1ChoseAdventurer = false;
+        }
+        else if (state == ChoicesStates.P2TURN)
+        {
+            P2ChoseAdventurer = false;
+        }
+    }
+
+    public void AdventurerChoice()
+    {
+        if (state == ChoicesStates.P1TURN)
+        {
+            P1ChoseAdventurer = true;
+        }
+        else if (state == ChoicesStates.P2TURN)
+        {
+            P2ChoseAdventurer = true;
+        }
     }
 }
